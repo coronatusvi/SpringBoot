718ee327d60c4342c8e21426a4923d8578244359
coronatusvi <vidangquang1203@gmail.com>
2024-07-23 11:50:00 +0700

    second commit

--- src/main/java/com/example/TestGit/TestGitApplication.java
+++ src/main/java/com/example/TestGit/TestGitApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestGitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestGitApplication.class, args);
+	public static void main(String[] autoconfigureArgs) throws Exception {
+		SpringApplication.run(TestGitApplication.class, autoconfigureArgs) throws Exception {);
 	}
 
 }
